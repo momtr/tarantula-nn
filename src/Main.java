07f76ad155f912cd31d1f7aab73ce296e9186b63
Moritz Mitterdorfer <47625175+moritzmitterdorfer@users.noreply.github.com>
2020-05-16 23:39:31 +0200

    👋 typo

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,9 @@ public class Main {
         data.addExample(new double[]{1,1}, new double[]{0});
         data.addExample(new double[]{0,0}, new double[]{0});
         data.addExample(new double[]{1,0}, new double[]{1});
-        data.addExample(new double[]{1,1}, new double[]{1});
+        data.addExample(new double[]{0,1}, new double[]{1});
 
-        // train model (stochastic gradient descnet)
+        // train model (stochastic gradient descent)
         nn.trainSGD(data, 10000, 0.2);
 
         // predict
